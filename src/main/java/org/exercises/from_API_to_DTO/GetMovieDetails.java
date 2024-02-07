cf1a94d041c85679d0dfdd0c1741c41d06a1e2eb
TRossen89 <110735176+TRossen89@users.noreply.github.com>
2024-02-07 20:28:11 +0100

    LocalDate exercise

--- src/main/java/org/exercises/from_API_to_DTO/GetMovieDetails.java
+++ src/main/java/org/exercises/from_API_to_DTO/GetMovieDetails.java
@@ -4,9 +4,10 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import com.google.gson.*;
-import lombok.*;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class GetMovieDetails {
 
@@ -15,44 +16,75 @@ public class GetMovieDetails {
     public static void main(String[] args) {
 
         GetMovieDetails getMovieDetails = new GetMovieDetails();
+
         String url = "https://api.themoviedb.org/3/find/{external_id}?external_source=imdb_id&language=english";
-
         String theShawShankRedemptionId = "tt0111161";
-        //getMovieDetails.getResponseBody(url);
-
-        // Test comment
+        //getMovieDetails.getResponseBodyById(url, theShawShankRedemptionId);
 
 
-        JsonObject overviewObject = getMovieDetails.getMovieOverview(url, theShawShankRedemptionId);
+        // Movie overview - 2 solutions
 
-        MovieDTO properMovieDT0 = new MovieDTO();
+        // - Solution 1
+        MediaDTO media = getMovieDetails.convertToMediaDTO(url, theShawShankRedemptionId);
 
-        properMovieDT0.setOverview(overviewObject.get("overview").getAsString());
+        MovieDTO movieDTO1 = media.movie_results.get(0);
 
-        System.out.println(properMovieDT0.overview);
+        System.out.println(movieDTO1.overview);
+
+
+        // - Solution 2
+        JsonObject overviewObject = getMovieDetails.getMovieOverviewInJsonObject(url, theShawShankRedemptionId);
+
+        MovieDTO movieDT02 = new MovieDTO();
+
+        movieDT02.setOverview(overviewObject.get("overview").getAsString());
+
+        System.out.println(movieDT02.overview);
 
 
 
-        /* System.out.println("Overview object: " + overviewObject.get("overview"));
+        // LocalDate
+        MovieDTO movieDTO3 = getMovieDetails.getMovieDTOWithLocalDate(url, theShawShankRedemptionId);
 
-        MovieDTO movieDTO = getMovieDetails.getMovieDetails(url, theShawShankRedemptionId);
-
-        System.out.println(movieDTO.overview);
-
-         */
-
+        System.out.println("Release year: " + movieDTO3.release_year +
+                "\nRelease date as local date: " + movieDTO3.release_date_as_local_date);
 
     }
 
-    private MovieDTO getMovieDetails(String url, String movieId){
-        String details = getResponseBody(url, movieId);
-        MovieDTO movieDetails = gson.fromJson(details, MovieDTO.class);
+    private MovieDTO getMovieDTOWithLocalDate(String url, String movieId){
+
+        MovieDTO movieDTOToReturn = new MovieDTO();
+
+        MediaDTO mediaDTO = convertToMediaDTO(url, movieId);
+
+        movieDTOToReturn = mediaDTO.movie_results.get(0);
+
+        // Getting release_date as LocalDate
+        String release_date = movieDTOToReturn.release_date;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-d");
+        LocalDate localDateOfMovie = LocalDate.parse(release_date, formatter);
+
+        // Getting release year as a String
+        String release_year = release_date.substring(0, 4);
+
+        // Setting release_date_as_local_date and release_year
+        movieDTOToReturn.setRelease_date_as_local_date(localDateOfMovie);
+        movieDTOToReturn.setRelease_year(release_year);
+
+        return movieDTOToReturn;
+    }
+
+    private MediaDTO convertToMediaDTO(String url, String movieId){
+        String details = getResponseBodyById(url, movieId);
+        MediaDTO movieDetails = gson.fromJson(details, MediaDTO.class);
+
         return movieDetails;
     }
 
-    private JsonObject getMovieOverview (String url, String movieId){;
 
-        String result = getResponseBody(url, movieId);
+    private JsonObject getMovieOverviewInJsonObject(String url, String movieId){;
+
+        String result = getResponseBodyById(url, movieId);
 
         JsonElement jsonElement = JsonParser.parseString(result);
 
@@ -74,12 +106,37 @@ public class GetMovieDetails {
             throw new RuntimeException("Not a json object");
         }
     }
-    private String getResponseBody(String url, String movieId){
+
+    private String getResponseBodyById(String url, String movieId){
+
         OkHttpClient client = new OkHttpClient();
 
         Request request = new Request.Builder()
                 .url(url
                         .replace("{external_id}", movieId))
+                .get()
+                .addHeader("accept", "application/json")
+                .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMWZjNjk2MDU3ZmNlZjExMTY3ZjUxY2ZhYzUyNzcxYiIsInN1YiI6IjY1YzIxMzAyOGU4ZDMwMDE3Yjc4YTJmZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hzZZ8YzDsNKpPdZMAUNd5VjLQ65blMh9UvHJbCOzc8s")
+                .build();
+
+        Response response = null;
+
+        try{
+            response = client.newCall(request).execute();
+            String result = response.body().string();
+            //System.out.println(result);
+            return result;
+        }
+        catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String getResponseBody(String url){
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
                 .get()
                 .addHeader("accept", "application/json")
                 .addHeader("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMWZjNjk2MDU3ZmNlZjExMTY3ZjUxY2ZhYzUyNzcxYiIsInN1YiI6IjY1YzIxMzAyOGU4ZDMwMDE3Yjc4YTJmZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hzZZ8YzDsNKpPdZMAUNd5VjLQ65blMh9UvHJbCOzc8s")
--- src/main/java/org/exercises/from_API_to_DTO/MovieDTO.java
+++ src/main/java/org/exercises/from_API_to_DTO/MovieDTO.java
@@ -1,12 +1,39 @@
 package org.exercises.from_API_to_DTO;
 
-import java.util.List;
+import java.time.LocalDate;
 
 public class MovieDTO {
     String overview;
+    String release_date;
+    transient LocalDate release_date_as_local_date;
+    transient String release_year;
 
     public String getOverview() {
         return overview;
+    }
+
+    public String getRelease_date() {
+        return release_date;
+    }
+
+    public void setRelease_date(String release_date) {
+        this.release_date = release_date;
+    }
+
+    public LocalDate getRelease_date_as_local_date() {
+        return release_date_as_local_date;
+    }
+
+    public void setRelease_date_as_local_date(LocalDate release_date_as_local_date) {
+        this.release_date_as_local_date = release_date_as_local_date;
+    }
+
+    public String getRelease_year() {
+        return release_year;
+    }
+
+    public void setRelease_year(String release_year) {
+        this.release_year = release_year;
     }
 
     public void setOverview(String overview) {
